d0502f673ac3ed833d3bcb8375903d6c51d85f81
RobinBroberg <robin.broberg86@gmail.com>
2025-01-27 16:45:29 +0100

    changed color of logo

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -13,7 +13,7 @@ export function Header() {
   return (
     <Navbar fluid className="bg-primary border-b border-black">
       <Navbar.Brand as={Link} href="/#" onClick={() => setCurrentPath("/")}>
-        <span className="self-center whitespace-nowrap text-xl font-semibold text-gray-text">
+        <span className="self-center whitespace-nowrap text-xl font-semibold bg-gradient-to-r from-purple-500 via-pink-500 to-pink-700 bg-clip-text text-transparent">
           Netflix Gemini
         </span>
       </Navbar.Brand>
